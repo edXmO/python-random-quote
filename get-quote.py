c078bf0203ff8f7e71301b1eb6afe6d9a3618e28
Eduardo <ex.romeroma@gmail.com>
2020-11-25 10:14:33 +0100

    Added cleanQuotes, which is an array without the \n (newline) character and print another quote

--- get-quote.py
+++ get-quote.py
@@ -5,10 +5,16 @@ def primary():
 
     f = open("python-random-quote/quotes.txt")
     quotes = f.readlines()
+    # Added cleanQuotes, which is an array without the \n (newline) character
+    cleanQuotes = []
+    for elem in range(0, len(quotes) - 1):
+        cleanQuotes.append(quotes[elem].rstrip("\n"))
+    print(cleanQuotes)
     f.close()
     last = 13
     rnd = random.randint(0, last)
-    print(quotes[rnd])
+    # Added printing another quote
+    print(cleanQuotes[rnd] + " " + cleanQuotes[rnd + 1])
 
 
 if __name__ == "__main__":
